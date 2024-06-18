62d365af1e214a1692daa34a37067c467588c563
Ernaud Piels <ernaudpiels@icloud.com>
2024-06-18 12:44:19 +0200

    ajout des composants header et footer et des routes pour les pages home panier galerie et mon compte

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./css/style.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import banner from "./assets/banner.jpg";
+import Galery from "./pages/Galery";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
 
-function App() {
-    return <></>;
+function App(): JSX.Element {
+    return (
+        <BrowserRouter>
+            <Header title="CheakyKicks" />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home
+                            title="Achetez"
+                            description="des chaussures en cuir"
+                            image={banner}
+                        />
+                    }
+                />
+                <Route path="/galerie" element={<Galery />} />
+                <Route path="/panier" element={<Cart />} />
+                <Route path="/mon-compte" element={<Account />} />
+            </Routes>
+            <Footer year={2024} companyName="CheakyKicks" />
+        </BrowserRouter>
+    );
 }
 
 export default App;
